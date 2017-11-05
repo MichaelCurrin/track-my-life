a153bcafdb37b481fe45a5a6f68990961714ebb8
MichaelCurrin <michael.currin@gmail.com>
2017-11-05 21:34:44 +0200

    Update trackMyLife.py to improve configuration handling and root handler.

--- app/trackMyLife.py
+++ app/trackMyLife.py
@@ -19,44 +19,54 @@ import cherrypy
 
 # Be careful when testing this line alone in python command line, as you are
 # working in '__main__' and the script will be imported as '__main__'.
-from lib import conf
+from lib import APP_DIR, conf
 from lib import database as db
 from services import setupSERVICES
-
-
-CONFIG_FILES = ('etc/cherrypy.conf', 'etc/cherrypy.local.conf')
 
 
 class Root(object):
     """
     Service for application root.
     """
-    @cherrypy.expose
-    def index(self):
-        """
-        Static home page.
 
-        TODO: Serve this and other pages off of root using the conf rather,
-        without ui in path.
+    ## TEMP
+    ## Leave this unexposed while in def, so that we get a 404 on a bad path.
+    ##@cherrypy.expose
+    def default(self, *args, **kwargs):
         """
-        return file('ui/index.html')
+        For paths which do not exactly match the ui HTML pages, redirect to home page.
+        """
+        raise cherrypy.HTTPRedirect('/', 302)
 
 
 # Only keep dropAll=True while in develop mode, to replace tables each time
 # but also delete all data.
 db.initialise(dropAll=True)
 
-cherrypy.root = Root()
-cherrypy.root.services = setupSERVICES()
+controllersConf = os.path.join(APP_DIR, 'etc', 'controllers.conf')
+controllersLocalConf = os.path.join(APP_DIR, 'etc', 'controllers.local.conf')
+servicesConf = os.path.join(APP_DIR, 'etc', 'services.conf')
+servicesLocalConf = os.path.join(APP_DIR, 'etc', 'services.local.conf')
 
-for c in CONFIG_FILES:
-    if os.access(c, os.R_OK):
-        cherrypy.config.update(c)
+webApp = cherrypy.tree.mount(Root(), '/', config=controllersConf)
+servicesApp = cherrypy.tree.mount(setupSERVICES(), '/services',
+                                  config=servicesConf)
+
+# If local config files exist, merge in the tree-specific sections for
+# the appropriate app.
+if os.access(controllersLocalConf, os.R_OK):
+    webApp.merge(controllersLocalConf)
+if os.access(servicesLocalConf, os.R_OK):
+    servicesApp.merge(servicesLocalConf)
+
+for c in ('http.conf', 'http.local.conf'):
+    path = os.path.join(APP_DIR, 'etc', c)
+    if os.access(path, os.R_OK):
+        cherrypy.config.update(path)
 
 # We can run the application on PythonAnywhere.com by importing this object
 # into the WSGI script.
-app = cherrypy.tree.mount(cherrypy.root)
-
+app = webApp
 
 if __name__ == '__main__':
     cherrypy.engine.start()
