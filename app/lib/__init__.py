ddcce7d54552c907aea3cf5a3d13b0355f97d718
MichaelCurrin <michael.currin@gmail.com>
2017-11-05 21:31:31 +0200

    Update lib/__init__.py app config handling

--- app/lib/__init__.py
+++ app/lib/__init__.py
@@ -1,8 +1,40 @@
 """
 Initialisation file for lib directory.
 """
+import os
 from ConfigParser import SafeConfigParser
 
-# Make app configuration file available.
-conf = SafeConfigParser()
-conf.read('etc/app.conf')
+# Available with `from lib import APP_DIR`.
+APP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+
+class AppConf(SafeConfigParser):
+    """
+    Subclassed config object with app configuration values.
+
+    On initilisation, reads values from two possible conf text files and
+    sets the app dir in DEFAULT section it can be subsituted throughout the
+    conf values.
+    """
+
+    CONF_NAMES = ('app.conf', 'app.local.conf')
+
+    def __init__(self):
+        """
+        Read and parse the global and local configs.
+        """
+        SafeConfigParser.__init__(self)
+
+        confPaths = [os.path.join(APP_DIR, 'etc', f) for f in
+                     self.__class__.CONF_NAMES]
+
+        # Raise error if main file does not exist.
+        assert os.access(confPaths[0], os.R_OK), (
+            'Cannot read config file: `{0}`.'.format(confPaths[0])
+            )
+
+        self.read(confPaths)
+        self.set('DEFAULT', 'appDir', APP_DIR)
+
+
+conf = AppConf()
